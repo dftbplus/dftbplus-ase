83aa95b6c8f99855e49053106c1b037f86d9b226
Tammo van der Heide <vanderhe@uni-bremen.de>
2022-05-13 14:24:09 +0200

    Kick off project and set up basic infrastructure

---
+++ src/dftbplus/__init__.py
@@ -0,0 +1,6 @@
+#------------------------------------------------------------------------------#
+#  dftbplus-ase: Interfacing DFTB+ with the Atomic Simulation Environment      #
+#  Copyright (C) 2006 - 2022  DFTB+ developers group                           #
+#                                                                              #
+#  See the LICENSE file for terms of usage and distribution.                   #
+#------------------------------------------------------------------------------#
